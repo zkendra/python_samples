aaaedc28e62599b4b6be425bd895c30530811f4d
Zack <zackary.kendra@gmail.com>
2019-08-25 14:47:09 -0400

    somewhat cleaned version

--- original/zip_image_search.py
+++ original/zip_image_search.py
@@ -1,27 +1,64 @@
 ##Intro code--import libraries and modules, load face detection classifier, import the small file to with which to practice,
 ##and then open the zip file and create a list of the files inside
+import zipfile
 
-
-#     file_name_str = 'readonly/small_img.zip'
-#     face_cascade_str = 'readonly/haarcascade_frontalface_default.xml'
+from PIL import Image
+from PIL import ImageDraw
+import pytesseract
+import cv2 as cv
+import numpy as np
 
 def main (name_str, file_name_str, face_cascade_str):
+	name_dictionary = setup_dictionary (file_name_str, face_cascade_str)
     
-    import zipfile
-
-    from PIL import Image
-    from PIL import ImageDraw
-    import pytesseract
-    import cv2 as cv
-    import numpy as np
-
-    file_name_str = file_name_str
-    face_cascade_str = face_cascade_str
+    print ("Starting to search_name_return_faces")
     
-    search_name_return_faces (name_str, file_name_str, face_cascade_str)
+    for name in name_dictionary:
+        text = name_dictionary[key][-1]
+        if (text.find(name_str)) != -1:
+            print ("{} in {} = true".format(name_str, name))
+            # create contact sheet from list of images (not perfect, needs work, and the height and width is hard-coded with 5 and 4.
+            # will need to determine the number of files in a list and probably update the sizes of the thmbnls)
+            count = len(name_dictionary[name][0])
+            
+            # create a contact sheet from images##
+            display(type(name_dictionary[name][0][0]))
+            first_image=name_dictionary[name][0][0]
+            first_image_copy = first_image.copy()
+            first_image_copy = first_image_copy.thumbnail ([100,100])
+
+            columns = int(count / 2)
+            rows = int (count / columns)
+            contact_sheet=Image.new(first_image.mode, (first_image.width*columns ,first_image.height*rows))
+            x=0
+            y=0
+
+            # read image(s) and convert to RGB
+            for img in name_dictionary[name][0]:
+                img_copy = img.copy()
+                img_copy = img.thumbnail([100,100])
+
+                # Lets paste the current image into the contact sheet
+                contact_sheet.paste(img, (x,y))
+
+                # Update our X position. If it is going to be the width of the image, then we set it to 0
+                if x+ first_image.width == contact_sheet.width:
+                    x=0
+                    # Update Y to point to the next "line" of the contact sheet.
+                    y= y + first_image.height
+                else:
+                    x = x + first_image.width
+
+            # display the contact sheet
+            contact_sheet.save("contact_sheet.jpg")
+            display(contact_sheet)   
+            
+        else:
+            print ("{} in {} = false".format(name_str, name))
+            
 
 #this function accepts a zipfile as a string and returns a list of the files, in this case, image files
-def create_list_unzipped_files(file_name_str):
+def get_files(file_name_str):
     print ("Starting to create_list_unzipped_files")
    
     # specifying the zip file name 
@@ -29,23 +66,16 @@ def create_list_unzipped_files(file_name_str):
   
     # opening the zip file in READ mode 
     with zipfile.ZipFile(file_name, 'r') as zip: 
-        # printing all the contents of the zip file 
-#         zip.printdir() 
-  
         # extracting all the files 
         print("Extracting all the files now...") 
         zip.extractall()
     
     #create a list of the files within the ZipFile
-    list_unzipped_files = zip.namelist()
+    files = zip.namelist()
    
-    print (("Number of files in zip folder is: ") + (str(len(zip.namelist()))))
-    
-    display (list_unzipped_files)
-    
-    return list_unzipped_files
-
-    print ("Done with create_list_unzipped_files!")
+    print (("Number of files in zip folder is: ") + str(len(files)))
+    display (files)
+    return files
 
 # #this function takes a list [(x,y,w,h),...] and returns [(x1, y1, x2, y2),....] which is more usable for creating a 
 # bounding_box by which the image is cropped and the resulting images are saved to a list 
@@ -92,9 +122,9 @@ def create_list_face_thmbnl_imgs(array_name, list_name):
 #It accepts the following parameters: zip file name as file_name_str, and the haar cascade as face_cascade_str
 # It stores the tuples in a key dictionary, the key = img_name, and the value = list of tuples
 #This function takes a LOOOONG time to run :(
-def create_dictionary_of_thmbnls_text (file_name_str, face_cascade_str):
+def setup_dictionary (file_name_str, face_cascade_str):
     
-    list_unzipped_files = create_list_unzipped_files (file_name_str)
+    files = get_files (file_name_str)
     
     print ("Starting to create_dictionary_of_thmbnls_text")
     
@@ -104,7 +134,7 @@ def create_dictionary_of_thmbnls_text (file_name_str, face_cascade_str):
     #iterate through image files and create arrays
     dictionary = { }
     
-    for img in list_unzipped_files:
+    for img in files:
         img_name = img
         img = cv.imread(img)
         gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -114,9 +144,6 @@ def create_dictionary_of_thmbnls_text (file_name_str, face_cascade_str):
         text = pytesseract.image_to_string(img)
         
         dictionary [img_name] = [create_list_face_thmbnl_imgs(gray, array_faces_list)] + [text] 
-        
-    # check to see that the dictionary was created correctly
-#     display (dictionary)
     
     print ('Done with create_dictionary_of_thmbnls_text!')   
     
@@ -125,54 +152,7 @@ def create_dictionary_of_thmbnls_text (file_name_str, face_cascade_str):
 
 def search_name_return_faces (name_str, file_name_str, face_cascade_str):
     
-    name_dictionary = create_dictionary_of_thmbnls_text (file_name_str, face_cascade_str)
     
-    print ("Starting to search_name_return_faces")
-    
-    for key in name_dictionary:
-        text = name_dictionary[key][-1]
-        if (text.find(name_str)) != -1:
-            print ("{} in {} = true".format(name_str, key))
-             #  #create contact sheet from list of images (not perfect, needs work, and the height and width is hard-coded with 5 and 4.
-             # #will need to determine the number of files in a list and probably update the sizes of the thmbnls)
-            count = len(name_dictionary[key][0])
-            
-            ##create a contact sheet from images##
-            display(type(name_dictionary[key][0][0]))
-            first_image=name_dictionary[key][0][0]
-            first_image_copy = first_image.copy()
-            first_image_copy = first_image_copy.thumbnail ([100,100])
-#             first_image.open().show()
-            columns = int(count / 2)
-            rows = int (count / columns)
-            contact_sheet=Image.new(first_image.mode, (first_image.width*columns ,first_image.height*rows))
-            x=0
-            y=0
-
-            # read image(s) and convert to RGB
-            for img in name_dictionary[key][0]:
-                img_copy = img.copy()
-                img_copy = img.thumbnail([100,100])
-            # Lets paste the current image into the contact sheet
-#                 img = Image.open(img)
-                contact_sheet.paste(img, (x,y))
-
-            # Now we update our X position. If it is going to be the width of the image, then we set it to 0
-            # and update Y as well to point to the next "line" of the contact sheet.
-                if x+ first_image.width == contact_sheet.width:
-                    x=0
-                    y= y + first_image.height
-                else:
-                    x = x + first_image.width
-
-            # resize and display the contact sheet
-#           contact_sheet = contact_sheet.resize((int(contact_sheet.width/2),int(contact_sheet.height/2) ))
-            contact_sheet.save("contact_sheet.jpg")
-            display(contact_sheet)   
-            
-        else:
-            print ("{} in {} = false".format(name_str, key))
-            
 
 
 main ('Snyder','readonly/small_img.zip','readonly/haarcascade_frontalface_default.xml')
